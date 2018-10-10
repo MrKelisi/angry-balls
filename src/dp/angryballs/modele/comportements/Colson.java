ba20418689d28d4ced974c2a10bbd14d212daa82
Florian Roméo <florian.romeo@feragon.net>
2018-10-10 20:08:18 +0200

    Ajout pauses

--- src/dp/angryballs/modele/comportements/Colson.java
+++ src/dp/angryballs/modele/comportements/Colson.java
@@ -7,6 +7,8 @@ import mesmaths.geometrie.base.Vecteur;
 import java.util.Random;
 
 public class Colson extends DecorateurBille {
+    private double tempsAvantPause;
+    private double tempsPause;
 
     public Colson(Bille billeDecoree) {
         super(billeDecoree);
@@ -14,8 +16,31 @@ public class Colson extends DecorateurBille {
 
     @Override
     public void deplacer(double deltaT) {
-        super.deplacer(deltaT);
         Random r = new Random();
-        getPosition().ajoute(new Vecteur(r.nextDouble() * 4 - 2, r.nextDouble() * 4 - 2));
+        Vecteur tremblement;
+
+        if(tempsAvantPause > 0) {
+            super.deplacer(deltaT);
+            tremblement = new Vecteur(r.nextDouble() * 4 - 2, r.nextDouble() * 4 - 2);
+            tempsAvantPause -= deltaT;
+        }
+        else if(tempsPause > 0) { //Bon, on va faire la pause
+            tempsPause -= deltaT;
+            tremblement = new Vecteur(0,0);
+        }
+        else {
+            tempsAvantPause = r.nextDouble() * 30000 + 30000;
+            tempsPause = r.nextDouble() * 10000 + 5000;
+            tremblement = new Vecteur(r.nextDouble() * 2 - 1, r.nextDouble() * 2 - 1);
+        }
+
+        getPosition().ajoute(tremblement);
     }
+
+    @Override
+    public Vecteur getVitesse() {
+        return super.getVitesse().produit(0.1);
+    }
+
+
 }
