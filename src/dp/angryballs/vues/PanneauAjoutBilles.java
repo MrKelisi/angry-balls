4df689bf415fcc7ba59c22a24240bdc32f594a78
Chris Webert <chris.webert.57@gmail.com>
2018-10-17 14:26:09 +0200

    ColorChooser + Slider rayon

--- src/dp/angryballs/vues/PanneauAjoutBilles.java
+++ src/dp/angryballs/vues/PanneauAjoutBilles.java
@@ -4,6 +4,8 @@ import dp.angryballs.Outils;
 import dp.angryballs.modele.DecorateurBille;
 
 import javax.swing.*;
+import javax.swing.colorchooser.AbstractColorChooserPanel;
+import javax.swing.colorchooser.ColorSelectionModel;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -11,6 +13,8 @@ import java.util.Collection;
 public class PanneauAjoutBilles extends Panel {
     private Label labelAjoutBilles;
     private ArrayList<BoutonComportement> listDecorators = new ArrayList<>();
+    private JColorChooser colorChooser = new JColorChooser(Color.BLACK);
+    private JSlider sliderRayon = new JSlider(15,100,30);
     private Button createButton;
 
     public PanneauAjoutBilles() {
@@ -20,6 +24,10 @@ public class PanneauAjoutBilles extends Panel {
 
         labelAjoutBilles = new Label("Création de billes");
         createButton = new Button("Créer");
+
+        AbstractColorChooserPanel panels[] = { };
+        //colorChooser.setChooserPanels(panels);
+        colorChooser.setPreviewPanel(new JPanel());
 
         try {
             Collection<Class<? extends DecorateurBille>> classes = Outils.getClasses("dp.angryballs.modele.comportements", DecorateurBille.class);
@@ -35,6 +43,8 @@ public class PanneauAjoutBilles extends Panel {
         for(BoutonComportement bc : listDecorators) {
             add(bc.getCheckbox());
         }
+        add(sliderRayon);
+        add(colorChooser);
         add(createButton);
     }
 
@@ -44,5 +54,13 @@ public class PanneauAjoutBilles extends Panel {
 
     public ArrayList<BoutonComportement> getDecorators() {
         return listDecorators;
+    }
+
+    public Color getColor() {
+        return colorChooser.getColor();
+    }
+
+    public double getRayon() {
+        return sliderRayon.getValue();
     }
 }
