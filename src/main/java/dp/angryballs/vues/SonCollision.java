977fc6be7c136a8ffe1f99049bc347bd5abbb0b0
Chris Webert <chris.webert.57@gmail.com>
2018-11-07 10:45:15 +0100

    Son stéréo!

--- src/main/java/dp/angryballs/vues/SonCollision.java
+++ src/main/java/dp/angryballs/vues/SonCollision.java
@@ -3,12 +3,9 @@ package dp.angryballs.vues;
 import dp.angryballs.CollisionObserver;
 import dp.angryballs.modele.Bille;
 import mesmaths.geometrie.base.Vecteur;
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
 
 import javax.sound.sampled.*;
 import java.io.File;
-import java.io.FileInputStream;
 
 public class SonCollision implements CollisionObserver {
     private static File son;
@@ -25,8 +22,6 @@ public class SonCollision implements CollisionObserver {
     @Override
     public void collides(Bille b1, Bille b2) {
         try {
-            double forceImpact = Math.max(b1.getVitesse().norme(), b2.getVitesse().norme());
-            float gainDecibel = (float) Math.min(forceImpact*8 - 30, 6.0206);   // Limite (-80, 6.0206) : POURQUOI ??
 
             AudioInputStream ais = AudioSystem.getAudioInputStream(son);
             Clip clip = AudioSystem.getClip();
@@ -35,7 +30,20 @@ public class SonCollision implements CollisionObserver {
             FloatControl master_gain = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
             FloatControl balance = (FloatControl) clip.getControl(FloatControl.Type.BALANCE);
 
+
+            double forceImpact = Math.max(b1.getVitesse().norme(), b2.getVitesse().norme());
+            float gainDecibel = (float) Math.min(forceImpact*8 - 30, 6.0206);   // Limite (-80, 6.0206) : POURQUOI ??
             master_gain.setValue(gainDecibel);
+
+
+            Vecteur positionImpact = b1.getPosition().somme(
+                    b2.getPosition().difference(b1.getPosition())
+                            .produit(b1.getRayon() / (b1.getRayon() + b2.getRayon()))
+            );
+            int fenetre = 1000;
+            double pos_y = Math.min(positionImpact.x, fenetre);
+            float bal = (float)pos_y / (float)(fenetre/2) - 1;
+            balance.setValue(bal);
 
             LineListener listener = lineEvent -> {
                 if (lineEvent.getType() != LineEvent.Type.STOP) {
@@ -47,6 +55,7 @@ public class SonCollision implements CollisionObserver {
 
             clip.addLineListener(listener);
             clip.start();
+
         }
         catch(Exception e){
             e.printStackTrace();
